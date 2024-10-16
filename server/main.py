d45af8d3eed1f1f00377423d6513eea77114ab5a
assaf <asssaf@localhost>
2024-10-15 19:14:28 -0700

    get number of emails sent since the beginning of the day

--- server/main.py
+++ server/main.py
@@ -1,14 +1,19 @@
 import datetime
 import email
+import io
 import json
 import os
+import sys
 import time
+import traceback
 
+import dateutil.tz
 from nacl.encoding import HexEncoder
 from nacl.signing import SigningKey, VerifyKey, SignedMessage
 from flask import abort, Flask, request
 
 from google.appengine.api import app_identity, mail, wrap_wsgi_app
+from google.cloud import monitoring_v3
 
 TIME_FUDGE_SECONDS = 120
 
@@ -39,7 +44,7 @@ def sendNotification():
     try:
         decoded = json.loads(msg)
 
-        subject = decoded['subject']
+        #subject = decoded['subject']
         body = decoded['body']
         timestamp = decoded['timestamp']
 
@@ -54,7 +59,7 @@ def sendNotification():
 
 
 
-    mime_message = email.message_from_string(body)
+    #mime_message = email.message_from_string(body)
 
     sender_address='noreply@{}.appspotmail.com'.format(app_identity.get_application_id())
     recipient_address=os.environ.get('SEND_TO')
@@ -68,6 +73,7 @@ def sendNotification():
     try:
         email_msg.Send()
         print("email sent")
+        notifySends()
     except Exception as e:
         print("error in send: ", e)
         abort(500)
@@ -86,6 +92,88 @@ def generateKey():
         "private_key": signing_key_bytes.decode('ascii'),
         "public_key": verify_key_bytes.decode('ascii')
     })
+
+
+@app.route('/health')
+def health():
+    return notifySends()
+
+
+def notifySends():
+    try:
+        results = getSuccessfulSends()
+
+        sends = results.points[0].value.int64_value
+        if sends in [60, 90, 120, 122]:
+            sender_address='noreply@{}.appspotmail.com'.format(app_identity.get_application_id())
+            email_msg = mail.AdminEmailMessage(
+                subject="appengine-mail-relay admin notification",
+                body="Messages sent at %d" % sends,
+                sender=sender_address,
+            )
+
+            try:
+                email_msg.Send()
+                print("admin email sent")
+                return json.dumps({})
+            except Exception as e:
+                print("error in admin send: ", e)
+                abort(500)
+
+    except Exception as e:
+        exc_info = sys.exc_info()
+        err = ''.join(traceback.format_exception(*exc_info))
+        return json.dumps({
+            "error": err,
+        })
+
+
+def getSuccessfulSends():
+    client = monitoring_v3.MetricServiceClient()
+    project_id = os.getenv("GOOGLE_CLOUD_PROJECT")
+    if not project_id:
+        abort(500)
+
+    project_name = f"projects/{project_id}"
+
+    tz = dateutil.tz.gettz('US/Pacific')
+    now = datetime.datetime.now(tz)
+    now_seconds = int(now.timestamp())
+    now_nanos = 0
+    midnight = now.replace(hour=0, minute=0, second=0, microsecond=0)
+    midnight_seconds = int(midnight.timestamp())
+    midnight_nanos = 0
+    interval = monitoring_v3.TimeInterval(
+        {
+            "end_time": {"seconds": now_seconds, "nanos": now_nanos},
+            "start_time": {"seconds": midnight_seconds, "nanos": midnight_nanos},
+        }
+    )
+    print("interval: ", interval)
+
+    # sum over the entire interval
+    aggregation_seconds = now_seconds - midnight_seconds
+
+    aggregation = monitoring_v3.Aggregation(
+        {
+            "alignment_period": {"seconds": aggregation_seconds},
+            "per_series_aligner": monitoring_v3.Aggregation.Aligner.ALIGN_SUM,
+            "group_by_fields": ["resource.zone"],
+        }
+    )
+
+    results = client.list_time_series(
+        request={
+           "name": project_name,
+           "filter": 'metric.type = "logging.googleapis.com/user/mail.send"',
+           "interval": interval,
+           "view": monitoring_v3.ListTimeSeriesRequest.TimeSeriesView.FULL,
+           "aggregation": aggregation,
+        }
+    )
+
+    for result in results:
+        return result
 
 
 if __name__ == '__main__':
